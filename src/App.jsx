6bad363c7279ba7d1ae301bfd30e36b1da132a60
maryamadel22 <maryamadel22>
2024-11-15 00:06:18 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,8 @@ import OtpScreen from './screens/OTP/otp';
 import EmailScreen from './screens/EmailInfo/email';
 import MainHome from './screens/HomePage/home';
 import useScreenStore from './store';
-import MainNotification from './components/notification/notification';
 const App=()=> {
-    const notifications = [
-        { name: 'Huda Salih', time: '7 months ago', imgSrc: '/h.png' },
-        { name: 'Ahmad Ali', time: '2 days ago', imgSrc: '/a.png' },
-        { name: 'Sara Khan', time: '5 hours ago', imgSrc: '/s.png' },
-      ];
+  
     const { currentScreen, setScreen } = useScreenStore();
     return (
         <div>
